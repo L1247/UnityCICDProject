3c85ca2241051183069cca94b15b906c7b07d08d
rStar <starlove124760@gmail.com>
2020-12-24 04:06:34 +0800

    add fail

--- Assets/Scripts/Tests/NewTestScript.cs
+++ Assets/Scripts/Tests/NewTestScript.cs
@@ -13,6 +13,7 @@ namespace Tests
         public void NewTestScriptSimplePasses()
         {
             // Use the Assert class to test conditions
+            Assert.Fail();
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
